5c39e483dfef67f047781cce4aad78742627db8e
vishant <vishant.96.sehrawat@gmail.com>
2023-08-26 03:10:16 +0530

    main image js bug fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
+// script.js
 const image = document.querySelector(".home-img");
 let scaleValue = 1;
 let opacityValue = 1;
-let touchStoppedTimer;
+let mouseStoppedTimer;
 let restoreTimer;
 
-const handleTouch = () => {
-  clearTimeout(touchStoppedTimer);
+document.addEventListener("mousemove", () => {
+  clearTimeout(mouseStoppedTimer);
   clearTimeout(restoreTimer);
 
+  // Set the initial opacity and scale values
   image.style.opacity = 1;
   image.style.transform = "translate(-50%, 0%) scale(1)";
 
+  // Decrease opacity and scale on mouse move
   opacityValue = 1;
   scaleValue = 1;
 
   const decreaseOpacityAndScale = () => {
-    opacityValue -= 0.02;
-    scaleValue -= 0.01;
+    opacityValue -= 0.02; // Adjust the decrement value for desired speed
+    scaleValue -= 0.01; // Adjust the decrement value for desired speed
 
     if (opacityValue > 0.5 && scaleValue > 0.9) {
       image.style.opacity = opacityValue.toFixed(2);
@@ -33,11 +36,12 @@ const handleTouch = () => {
   };
   requestAnimationFrame(decreaseOpacityAndScale);
 
-  touchStoppedTimer = setTimeout(() => {
+  // Set the timer to restore opacity and scale when the mouse stops moving
+  mouseStoppedTimer = setTimeout(() => {
     restoreTimer = setTimeout(() => {
       const restoreOpacityAndScale = () => {
-        opacityValue += 0.02;
-        scaleValue += 0.01;
+        opacityValue += 0.02; // Adjust the increment value for desired speed
+        scaleValue += 0.01; // Adjust the increment value for desired speed
 
         if (opacityValue < 1 && scaleValue < 1) {
           image.style.opacity = opacityValue.toFixed(2);
@@ -53,11 +57,9 @@ const handleTouch = () => {
         }
       };
       requestAnimationFrame(restoreOpacityAndScale);
-    }, 500);
-  }, 500);
-};
-
-document.addEventListener("touchstart", handleTouch);
+    }, 500); // Set the delay for restoration in milliseconds (1000ms = 1 second)
+  }, 500); // Set the delay for detecting mouse stop in milliseconds (1000ms = 1 second)
+});
 
 // navbar hamburger toggle ++++++++++++++++++++++++++++
 // ... Your existing script ...
