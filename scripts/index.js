fc08d01bea9d8b7463b51b32d51ccfb22d7336c6
vishant <vishant.96.sehrawat@gmail.com>
2023-08-26 03:07:04 +0530

    major responsiveness changes to header and main image

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,77 @@
-// script.js
-const image = document.querySelector('.home-img');
+const image = document.querySelector(".home-img");
+let scaleValue = 1;
 let opacityValue = 1;
-let mouseStoppedTimer;
+let touchStoppedTimer;
+let restoreTimer;
 
-document.addEventListener('mousemove', (e) => {
-    clearTimeout(mouseStoppedTimer);
+const handleTouch = () => {
+  clearTimeout(touchStoppedTimer);
+  clearTimeout(restoreTimer);
 
-    // Set the initial opacity to 1
-    image.style.opacity = 1;
+  image.style.opacity = 1;
+  image.style.transform = "translate(-50%, 0%) scale(1)";
 
-    // Decrease opacity to 0.5 on mouse move
-    opacityValue = 1;
-    const decreaseOpacity = () => {
-        opacityValue -= 0.02; // Adjust the decrement value for desired speed
-        if (opacityValue > 0.5) {
-            image.style.opacity = opacityValue.toFixed(2);
-            requestAnimationFrame(decreaseOpacity);
+  opacityValue = 1;
+  scaleValue = 1;
+
+  const decreaseOpacityAndScale = () => {
+    opacityValue -= 0.02;
+    scaleValue -= 0.01;
+
+    if (opacityValue > 0.5 && scaleValue > 0.9) {
+      image.style.opacity = opacityValue.toFixed(2);
+      image.style.transform = `translate(-50%, 0%) scale(${scaleValue.toFixed(
+        2
+      )})`;
+      requestAnimationFrame(decreaseOpacityAndScale);
+    } else {
+      opacityValue = 0.5;
+      scaleValue = 0.9;
+      image.style.opacity = opacityValue;
+      image.style.transform = `translate(-50%, 0%) scale(${scaleValue})`;
+    }
+  };
+  requestAnimationFrame(decreaseOpacityAndScale);
+
+  touchStoppedTimer = setTimeout(() => {
+    restoreTimer = setTimeout(() => {
+      const restoreOpacityAndScale = () => {
+        opacityValue += 0.02;
+        scaleValue += 0.01;
+
+        if (opacityValue < 1 && scaleValue < 1) {
+          image.style.opacity = opacityValue.toFixed(2);
+          image.style.transform = `translate(-50%, 0%) scale(${scaleValue.toFixed(
+            2
+          )})`;
+          requestAnimationFrame(restoreOpacityAndScale);
         } else {
-            opacityValue = 0.5;
-            image.style.opacity = opacityValue;
+          opacityValue = 1;
+          scaleValue = 1;
+          image.style.opacity = opacityValue;
+          image.style.transform = `translate(-50%, 0%) scale(${scaleValue})`;
         }
-    };
-    requestAnimationFrame(decreaseOpacity);
+      };
+      requestAnimationFrame(restoreOpacityAndScale);
+    }, 500);
+  }, 500);
+};
 
-    // Set the timer to restore opacity when the mouse stops moving
-    mouseStoppedTimer = setTimeout(() => {
-        const restoreOpacity = () => {
-            opacityValue += 0.02; // Adjust the increment value for desired speed
-            if (opacityValue < 1) {
-                image.style.opacity = opacityValue.toFixed(2);
-                requestAnimationFrame(restoreOpacity);
-            } else {
-                opacityValue = 1;
-                image.style.opacity = opacityValue;
-            }
-        };
-        requestAnimationFrame(restoreOpacity);
-    }, 500); // Set the delay in milliseconds (1000ms = 1 second)
+document.addEventListener("touchstart", handleTouch);
 
+// navbar hamburger toggle ++++++++++++++++++++++++++++
+// ... Your existing script ...
+
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+const navElements = document.querySelectorAll(".nav__item");
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
 });
+navElements.forEach((navElement) => {
+  navElement.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+});
+
+// navbar hamburger toggle ----------------------
